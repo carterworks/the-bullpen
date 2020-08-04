78a9e26b4738a00eecc59c10d1a454b8476c5a01
Carter McBride <junk@carter.works>
2020-08-04 16:56:11 -0600

    initial - configure purgecss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
