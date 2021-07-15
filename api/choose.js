0a7d7761c6cff2abe6ae195d4f1180b9d82be894
Carter McBride <junk@carter.works>
2021-07-14 20:40:11 -0600

    Create the no-js version of the picker

--- api/choose.js
+++ api/choose.js
@@ -1,41 +1,43 @@
-const { teams } = require("./get-teams");
+const { teams } = require('./get-teams');
 
 // get int in range [min, max]
 function randInt(max, min = 0) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function chooseRandomElement(choices) {
-    if (choices.length === 0) {
-        throw "Choices cannot be empty";
-    }
-    return choices[randInt(choices.length - 1, 0)];
+  if (choices.length === 0) {
+    throw 'Choices cannot be empty';
+  }
+  return choices[randInt(choices.length - 1, 0)];
 }
 
 exports.handler = async (event) => {
-    const teamName = event.queryStringParameters?.team?.toLowerCase();
-    let choices;
-    if (teamName) {
-        choices = teams[teamName];
+  const teamName = event.queryStringParameters?.team?.toLowerCase();
+  let choices;
+  if (teamName) {
+    choices = teams[teamName];
+  } else {
+    choices = event.queryStringParameters?.choices?.split(',');
+  }
+  if (!choices || choices.length === 0) {
+    const validTeamNames = Object.keys(teams)
+      .map((s) => `"${s}"`)
+      .join(', ');
+    let body;
+    if (teamName && !teams[teamName] && !event.queryStringParameters?.choices) {
+      body = `Name "${teamName}" is an invalid team name. Valid names are: ${validTeamNames}`;
     } else {
-        choices = event.queryStringParameters?.choices?.split(",");
+      body = `Missing required query parameters. Must have "team" (valid values are: ${validTeamNames}) or "choices" (valid values is a comma-separated, like "Alpha,Bravo,Charlie")`;
     }
-    if (!choices || choices.length === 0) {
-        const validTeamNames = Object.keys(teams).map(s => `"${s}"`).join(", ");
-        let body
-        if (!teams[teamName]) {
-            body = `Name "${teamName}" is an invalid team name. Valid names are: ${validTeamNames}`
-        } else {
-            body = `Missing required query parameters. Must have "team" (valid values are: ${validTeamNames}) or "choices" (valid values is a comma-separated, like "Alpha,Bravo,Charlie")`
-        }
-        return {
-            statusCode: 400,
-            body
-        }
-    }
-
     return {
-        statusCode: 200,
-        body: chooseRandomElement(choices)
-    }
-}
+      statusCode: 400,
+      body,
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: chooseRandomElement(choices),
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import Alpine from 'alpinejs';
 
+function removeNoJSOnlyElements() {
+  document.querySelectorAll('.no-js-only').forEach((el) => el.remove());
+}
+
+removeNoJSOnlyElements();
+window.Alpine = Alpine;
+Alpine.start();
